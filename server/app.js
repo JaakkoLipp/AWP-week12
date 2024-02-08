b7b764966848ff395bbdf64a5c3c18cf99d7ddcf
Jaakko <jaakko.lipponen01@gmail.com>
2024-02-08 10:19:20 +0200

    fixed missing else

--- server/app.js
+++ server/app.js
@@ -23,9 +23,7 @@ if (process.env.NODE_ENV === "production") {
   app.get("*", (req, res) =>
     res.sendFile(path.resolve(".", "client", "build", "index.html"))
   );
-}
-
-if (process.env.NODE_ENV === "development") {
+} else if (process.env.NODE_ENV === "development") {
   var corsOptions = {
     origin: "http://localhost:3000",
     optionsSuccessStatus: 200,
