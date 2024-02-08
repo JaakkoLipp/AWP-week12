3e840ab397dc658613ce689bb205267f51a87119
Jaakko <jaakko.lipponen01@gmail.com>
2024-02-08 09:56:10 +0200

    fix api path to book.js

--- server/app.js
+++ server/app.js
@@ -6,7 +6,7 @@ var logger = require("morgan");
 var cors = require("cors");
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
-var apiRouter = require("./api");
+var apiRouter = require("./api/book");
 var app = express();
 
 app.use(logger("dev"));
