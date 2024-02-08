098c2c6110c74c0b1024899774688414d9c6eada
Jaakko <jaakko.lipponen01@gmail.com>
2024-02-08 09:50:24 +0200

    route to routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import BookForm from "./Form";
 import BookInfo from "./BookInfo";
 import NotFound from "./NotFound"; // Import the NotFound component
@@ -8,9 +8,11 @@ function App() {
   return (
     <Router>
       <h1>Books</h1> {}
-      <Route path="/" exact component={BookForm} />
-      <Route path="/book/:bookName" component={BookInfo} />
-      <Route path="*" component={NotFound} />
+      <Routes>
+        <Route path="/" element={<BookForm />} />
+        <Route path="/book/:bookName" element={<BookInfo />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </Router>
   );
 }
